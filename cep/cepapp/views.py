edfab6ff07468fec01927ed38d0df72a0ff5b043
Pedro Henrique Melo Matos <melomatosp@gmail.com>
2025-07-26 11:34:18 -0300

    adicionando forms

--- cep/cepapp/views.py
+++ cep/cepapp/views.py
@@ -6,3 +6,8 @@ from django.http import HttpRequest
 def home(request:HttpRequest):
     if request.method == 'GET':
         return render(request, 'home.html')
+    elif request.method == 'POST':
+        inputcep = request.POST.get('cepinput') #pegando as infos do nosso input que veio do post lá do HTML
+        infos_endereco = f'viacep.com.br/ws/{inputcep}/json/'
+        print(infos_endereco)
+        return render(request, 'home.html')
